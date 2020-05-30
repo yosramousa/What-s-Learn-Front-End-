0234d492940968d7efe92ef5439fb2c85aeccf40
saraNegm <sarra.negm@gmail.com>
2020-05-30 17:45:46 +0200

    fixing

--- Our Final Project/First Version3/src/app/pages/inbox/inbox.component.ts
+++ Our Final Project/First Version3/src/app/pages/inbox/inbox.component.ts
@@ -12,4 +12,5 @@ export class InboxComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  InboxMessages:{}
 }
--- Our Final Project/First Version3/src/environments/environment.ts
+++ Our Final Project/First Version3/src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  api_url :"https://localhost:44319/"  
+  api_url :"http://localhost:51121/"  
 };
